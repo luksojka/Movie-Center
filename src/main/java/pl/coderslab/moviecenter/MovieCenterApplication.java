7da85c43b4fce2240dee493a8aab7b4791e5774d
Łukasz Sójka <lsojka93@gmail.com>
2020-10-18 18:46:09 +0200

    Project setup

---
+++ src/main/java/pl/coderslab/moviecenter/MovieCenterApplication.java
@@ -0,0 +1,13 @@
+package pl.coderslab.moviecenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/coderslab/moviecenter/MovieCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.moviecenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
